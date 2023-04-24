caca7d66f82af44661c433a281cf7d63f6f6e867
yatamanaa <yatamanaabai@gmail.com>
2023-04-24 12:35:16 +0600

    def ready in apps.py

--- weather/apps.py
+++ weather/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+from weather.management.commands.bot import main
 
 class WeatherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'weather'
+    def ready(self):
+        main()
